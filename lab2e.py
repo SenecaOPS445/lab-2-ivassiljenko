f2ce96282e43a4a03f7c926b8bc151161354b0d8
Ivan Vassiljenko <ivassiljenko@myseneca.ca>
2024-05-24 19:23:53 -0400

    Individual message or note.

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: [Ivan Vassiljenko]
+# Author ID: [ivassiljenko]
+# Date Created: [2024/05/24]
+
+timer = 10
+
+while timer != 0:
+   print(timer)
+   timer = timer -1
+
+print('blast off!')
